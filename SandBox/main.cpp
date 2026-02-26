49265eb5f248032586f733387a77206597893bf7
심재천 <shimnaldo98@gmail.com>
2026-02-26 18:32:10 +0900

    feat : OpenGL 연동

--- SandBox/main.cpp
+++ SandBox/main.cpp
@@ -1,7 +1,38 @@
 #include "MyForm.h"
+#include <windows.h>
+#include <GL/GL.h>
+#include <GL/GLU.h>
 
 using namespace System;
 using namespace System::Windows::Forms;
+
+HDC hDC;	// 윈도우 핸들
+HGLRC hRC;	// OpenGL 컨텍스트 전역
+
+void SetupOpenGL(HWND hWnd) {
+	hDC = GetDC(hWnd);		// 디바이스 컨텍스트 할당
+
+	// 픽셀 포맷 구조체 옵션
+	PIXELFORMATDESCRIPTOR pfd = { 0 };
+	pfd.nSize = sizeof(pfd);
+	pfd.nVersion = 1;
+
+	// 상세 속성 (Quality Settings)
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW	// 메모리가 아닌 윈도우 창에 렌더링
+		| PFD_SUPPORT_OPENGL			// OpenGL 명령어 지원
+		| PFD_DOUBLEBUFFER;				// 더블 버퍼링 (깜빡임 방지)
+	pfd.iPixelType = PFD_TYPE_RGBA;		// 색상 모델 : RGBA
+	pfd.cColorBits = 32;				// 색상 표현 : 32bit
+	pfd.cDepthBits = 24;				// Depth(Z) 버퍼 비트 : 24비트
+
+	// 윈도우 포맷 설정
+	int format = ChoosePixelFormat(hDC, &pfd);	
+	SetPixelFormat(hDC, format, &pfd);
+
+	// 그래픽 명령을 hDC의 RC에 적용
+ 	hRC = wglCreateContext(hDC);
+	wglMakeCurrent(hDC, hRC);
+}
 
 void OnIdle(Object^ sender, EventArgs^ e) {
 	// todo : 본 루프 구현
